7e795de9a284999620362dee027e7259767b5536
Shashank <shashankkaushik0718@gmail.com>
2026-01-13 18:12:15 +0530

    Remove crashing route

--- server/server.js
+++ server/server.js
@@ -58,17 +58,5 @@ app.listen(PORT, () => {
 });
 
 const path = require("path");
-
-
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/build", "index.html"));
-  });
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running..");
-  });
-}
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '../client/build')));
