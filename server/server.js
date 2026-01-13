64cc50541e13aaaa349de031739ad7e68eae8e60
Shashank <shashankkaushik0718@gmail.com>
2026-01-13 18:00:37 +0530

    Configure server to show frontend

--- server/server.js
+++ server/server.js
@@ -56,3 +56,19 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+const path = require("path");
+
+__dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
